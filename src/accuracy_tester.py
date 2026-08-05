5196838c44241d6b52983fac5b5bd176b00d9734
Daan Eeckloo <daan.eeckloo@gmail.com>
2026-08-05 10:24:26 +0200

    added a script to test the accuracy of models

---
+++ src/accuracy_tester.py
@@ -0,0 +1,21 @@
+import numpy as np
+import nnlib as nn
+from utils import load_mnist_csv
+
+network = nn.Network(nn.Losses.softmax_cross_entropy, 784, 128, 2, 10)
+network.add(nn.PreTrainedLayer("../models/v2/layer_0_weights.npy", "../models/v2/layer_0_biases.npy", nn.Activations.relu))
+network.add(nn.PreTrainedLayer("../models/v2/layer_1_weights.npy", "../models/v2/layer_1_biases.npy", nn.Activations.relu))
+network.add(nn.PreTrainedLayer("../models/v2/layer_2_weights.npy", "../models/v2/layer_2_biases.npy", nn.Activations.linear))
+
+images, labels = load_mnist_csv("../data/mnist_test.csv")
+
+total_score = 0
+
+for i in range(images.shape[0]):
+    prediction = network.forward(images[0]) #np.array([0.05, 0.05, ..., 0.85])
+    if np.argmax(prediction) == np.argmax(labels[i]):
+        total_score += 1
+
+accuracy = total_score / images.shape[0]
+
+print(f"accuracy: {accuracy}")
